64e92a06e5c1d02a5c8c5a2f11121ef8e94b39a5
Michele Puricelli <michele.puricelli89@gmail.com>
2022-05-10 15:02:53 +0200

    first commit with made layout

--- src/App.js
+++ src/App.js
@@ -1,9 +1,35 @@
-
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="h-screen w-screen">
+      {/* <header>
+        <div className='w-full container mx-auto flex flex-row justify-between p-4 bg-background-accent text-text_dark' >
+          <div>
+            NFsTore
+          </div>
+          <div className='container mx-auto flex flex-row justify-end'>
+            <div className='mx-1'>
+              Mint
+            </div>
+            <div className='mx-1'>
+              Social
+            </div>
+          </div>
+        </div>
+      </header> */}
+      <main className='h-full grid grid-cols-4 grid-rows-6'>
+        <div id='title' className='col-span-3 row-span-4 bg-background-accent'>
+          <h1 className=''>NFsTore</h1>
+        </div>
+        <div id='description' className='col-span-1 row-span-4 bg-background-main' >
+          Qui facciamo cose per vedere altre cose.
+        </div>
+        <div id='slideshow' className='col-span-4 bg-background-main row-span-2' >
+          Qui ci andranno le immagini
+        </div>
+
+      </main>
     </div >
   );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    colors: {
+      'text_dark': '#3f0d0c',
+      'text_light': '#d8d0c5',
+      'background-main': '#d9c4a9',
+      'background-accent': '#d9b061',
+      'accent': '#8d6f57'
+    },
+    extend: {},
+  },
+  plugins: [],
+}
