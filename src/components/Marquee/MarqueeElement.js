05ff00aef7d154ca8d25f34409275ae14ac2d3c6
Michele Puricelli <michele.puricelli89@gmail.com>
2022-05-17 10:01:35 +0200

    imported images in file

--- src/components/Marquee/MarqueeElement.js
+++ src/components/Marquee/MarqueeElement.js
@@ -2,8 +2,8 @@ import React from 'react'
 
 const MarqueeElement = (props) => {
     return (
-        <div>
-            <img src={props.src} alt={props.alt} title={props.title} className='md:w-1/4 md:h-1/4 w-1/5 h-1/5' />
+        <div className='max-w-[290px]'>
+            <img src={props.src} alt={props.alt} title={props.title} />
         </div>
     )
 }
