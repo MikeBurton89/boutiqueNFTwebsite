65a8bf5d433c1babd408e86eb948868ead1936e0
Michele Puricelli <michele.puricelli89@gmail.com>
2022-05-17 15:33:30 +0200

    added animation

--- src/components/Marquee/MarqueeSection.js
+++ src/components/Marquee/MarqueeSection.js
@@ -8,7 +8,7 @@ const MarqueeSection = () => {
     return (
         <section id='slideshow' className='col-span-1 row-span-2 md:col-span-4 md:row-span-2 bg-background-main border-t border-text_dark' >
             <Fade>
-                <Marquee speed={20} gradient={false}>
+                <Marquee delay={1} speed={25} gradient={false}>
                     {imagesData.map((image) => <MarqueeElement key={image.name} src={image.src} title={image.title} alt={image.alt} loading='lazy' />)}
                 </Marquee>
             </Fade>
